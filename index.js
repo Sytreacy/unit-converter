9a79d0f18d7df097a022f1ab0730f8e6fe633aca
Win Maw <sytreacy@gmail.com>
2022-10-02 20:22:45 +0630

    set default unit value

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ round the num down to 3 decimal places
 1 kilogram = 2.204 pound
 */
 let inputNum = document.getElementById("unit-input-field");
-let num = document.getElementById("unit-input-field").value;
-
 let meterFeetUnit = document.getElementById("meterFeetUnit");
 let convertVolumeUnit = document.getElementById("convertVolume");
 let convertMassUnit = document.getElementById("convertMass");
@@ -15,36 +13,38 @@ let result1;
 let result2;
 let resultUnit;
 
+inputNum.addEventListener("input", function () {
+  inputNum.value = inputNum.value;
+});
+
 function meterFeet() {
-  result1 = num * 3.281;
-  result2 = num / 3.281;
-  resultUnit = `${num} meters = ${Number(result1).toFixed(
+  result1 = inputNum.value * 3.281;
+  result2 = inputNum.value / 3.281;
+  resultUnit = `${inputNum.value} meters = ${Number(result1).toFixed(
     3
-  )} feet | ${num} feet = ${Number(result2).toFixed(3)} meters`;
+  )} feet | ${inputNum.value} feet = ${Number(result2).toFixed(3)} meters`;
   return resultUnit;
 }
 
 function convertVolume() {
-  result1 = num * 0.264;
-  result2 = num / 0.264;
-  resultUnit = `${num} liters = ${Number(result1).toFixed(
+  result1 = inputNum.value * 0.264;
+  result2 = inputNum.value / 0.264;
+  resultUnit = `${inputNum.value} liters = ${Number(result1).toFixed(
     3
-  )} gallons | ${num} gallons = ${Number(result2).toFixed(3)} liters`;
+  )} gallons | ${inputNum.value} gallons = ${Number(result2).toFixed(
+    3
+  )} liters`;
   return resultUnit;
 }
 
 function convertMass() {
-  result1 = num * 2.204;
-  result2 = num / 2.204;
-  resultUnit = `${num} kilos = ${Number(result1).toFixed(
+  result1 = inputNum.value * 2.204;
+  result2 = inputNum.value / 2.204;
+  resultUnit = `${inputNum.value} kilos = ${Number(result1).toFixed(
     3
-  )} pounds | ${num} pounds = ${Number(result2).toFixed(3)} kilos`;
+  )} pounds | ${inputNum.value} pounds = ${Number(result2).toFixed(3)} kilos`;
   return resultUnit;
 }
-
-inputNum.addEventListener("input", function () {
-  num = inputNum.value;
-});
 
 function convertUnits() {
   meterFeetUnit.textContent = meterFeet();
@@ -54,3 +54,6 @@ function convertUnits() {
 
 let convertBtn = document.getElementById("convert-btn");
 convertBtn.addEventListener("click", convertUnits);
+
+inputNum.value = 20;
+convertUnits();
